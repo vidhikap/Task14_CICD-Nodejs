50d5987bdd079316d8d683a1c79b17b65d118bf2
Vidhi Kapadia <48359421+vidhikap@users.noreply.github.com>
2022-09-02 20:05:50 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+#install nginx
+sudo apt-get update
+sudo apt-get install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
 #install node modules
 npm install
 
