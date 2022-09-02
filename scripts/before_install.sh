26930929ed8a76228b05feb298619893395b3e78
Vidhi Kapadia <48359421+vidhikap@users.noreply.github.com>
2022-09-02 20:27:08 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get -y update
+apt-get -y install nginx
+
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
