89045ff1431baaa9a07af52809141f7efc442ef4
Vidhi Kapadia <48359421+vidhikap@users.noreply.github.com>
2022-09-02 22:41:40 +0530

    Update nginxconf.sh

--- scripts/nginxconf.sh
+++ scripts/nginxconf.sh
@@ -1,4 +1,4 @@
 cd /etc/nginx/sites-available/
 sudo rm -rf default
-cp /home/ubuntu/app/default /etc/nginx/sites-available/
+sudo cp /home/ubuntu/app/default /etc/nginx/sites-available/
 sudo systemctl restart nginx
