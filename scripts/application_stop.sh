3e87c04e7e262d03dbb29c8739a2d8fea36516c6
Vidhi Kapadia <48359421+vidhikap@users.noreply.github.com>
2022-09-02 21:50:23 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,10 +2,4 @@
 #Stopping existing node servers
 echo "Stopping any existing node servers"
 pkill node
-echo "Checking for existing node process"
-ps -ef | grep nginx
-netstat -unltp | grep 80
-systemctl status nginx
-ls /etc/nginx
-pkill nginx
-echo "hi"
+
